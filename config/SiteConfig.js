6dbde72362a688998d5ce555eb4b109b413b2d86
J.D. Sandifer <sandifer.jd@gmail.com>
2018-11-08 17:32:53 -0800

    Fix links and tweak copy

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -1,13 +1,13 @@
 module.exports = {
-  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+  pathPrefix: 'blog', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
   siteTitle: 'The Blog of J.D. Sandifer', // Navigation and Site Title
   siteTitleAlt: 'My Development Blog - J.D. Sandifer', // Alternative Site title for SEO
-  siteUrl: 'https://jdsandifer.github.io/blog/', // Domain of your site. No trailing slash!
+  siteUrl: 'https://jdsandifer-blog.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
-  siteDescription: 'My blog about software development, LEGO, life, and whatever comes to mind.', // Your site description
+  siteDescription: 'My blog about software development, life, and whatever comes to mind.', // Your site description
   author: 'J.D. Sandifer', // Author for schemaORGJSONLD
   siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
 
